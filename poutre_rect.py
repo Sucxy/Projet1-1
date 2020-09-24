0c960d1790ef5c8d90a0dcc9fd1f59620b12c83b
jerobitaille <jerome.robitaille@polymtl.ca>
2020-09-24 22:26:51 +0000

    test_print

--- poutre_rect.py
+++ poutre_rect.py
@@ -1,3 +1,5 @@
+import math
+
 # Initialisation des variables
 
 F = 10000  # en N
@@ -5,12 +7,12 @@ E = 210  # en GPa = 10^3 N/mm^2
 L = 100  # en mm
 b = 10  # en mm
 h = 20  # en mm
-
+print F
 # Calcul de l'inertie
-
 I = (b*(h**3))/12
+print(I)
 
 # Calcul de la déformation maximale
 
 delta_max = (F*(L**3))/(3*E*I)
-
+print(delta_max)
